ca92aa3dc7b6aa0bfd6920d30426aaf43f5424c6
Juliana Cotabarren <julianacotabarren@gmail.com>
2023-07-15 19:37:54 -0300

    feat: add basic Modal

---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,17 @@
+
+
+const Modal = () => {
+  return (
+    <div>
+        <button>close</button>
+        <h1>Hero name</h1>
+        <div>
+            <img src="" alt="Comic img" />
+            <h2>Comic title</h2>
+            <p>Publication date</p>
+        </div>
+    </div>
+  )
+}
+
+export default Modal
