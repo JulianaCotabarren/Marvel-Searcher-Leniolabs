d98d8f14f8ccae21954368bdf5c51089872c70d6
Juliana Cotabarren <julianacotabarren@gmail.com>
2023-07-16 10:59:41 -0300

    feat: add star to Card and show favorite in console

---
+++ src/components/Card/styles/CardStarWrapper.jsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const CardStarWrapper = styled.div`
+    background: transparent;
+    position: absolute;
+    top: 0;
+    right: 10px;
+`;
+
+export default CardStarWrapper;
