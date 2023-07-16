3eda0387edc90934b3cd16497dbe0b0e2ceb63b8
Juliana Cotabarren <julianacotabarren@gmail.com>
2023-07-16 16:16:28 -0300

    feat: add useComicDetail

--- src/context/ComicDetailContext.jsx
+++ src/context/ComicDetailContext.jsx
@@ -1,28 +1,30 @@
-import axios from "axios";
-import { useEffect } from "react";
-import { useState } from "react";
 import { createContext } from "react"
-import { API_URL, AUTH_QUERIES } from "../constants";
+import useComicDetail from "../hooks/useComicDetail";
 
 export const ComicDetailContext = createContext();
 
 const ComicDetailProvider = ({children}) => {
-    const [loading, setLoading] = useState(false);
-    const [comicId, setComicId] = useState(null);
-    const [comicDetail, setcomicDetail] = useState(null);
-
-    useEffect(() => {
-        const getComicById = async() => {
-            const comicDetailResponse = await axios.get(`${API_URL}/comics/${comicId}?${AUTH_QUERIES}`);
-            setLoading(false);
-            setcomicDetail(comicDetailResponse.data.data.results[0]);
-        };
-        if (comicId) getComicById();
-    }, [comicId]);
+  const { 
+    loading,
+    comicId,
+    comicDetail,
+    setLoading,
+    setComicId,
+    setcomicDetail
+} = useComicDetail();    
 
   return (
-    <ComicDetailContext.Provider value={{ loading, comicId, comicDetail, setComicId, setLoading }}>
-        {children}
+    <ComicDetailContext.Provider 
+      value={{ 
+          loading,
+          comicId,
+          comicDetail,
+          setLoading,
+          setComicId,
+          setcomicDetail 
+      }}
+    >
+      {children}
     </ComicDetailContext.Provider>
   )
 }
