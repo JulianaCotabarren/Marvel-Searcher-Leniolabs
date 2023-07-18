1f8f0937c95122ef98ad18494563b27829d69612
Juliana Cotabarren <julianacotabarren@gmail.com>
2023-07-18 09:24:54 -0300

    feat: add default image for image_not_available

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -7,6 +7,7 @@ import CardImageWrapper from './styles/CardImageWrapper';
 import CardTitleWrapper from './styles/CardTitleWrapper';
 import CardWrapper from './styles/CardWrapper';
 import CardStarWrapper from './styles/CardStarWrapper';
+import marvelCharacters from '../../assets/images/marvelCharacters.jpg';
 
 const Card = ({ character }) => {
   const { id, name, thumbnail, comics } = character;
@@ -15,6 +16,11 @@ const Card = ({ character }) => {
   const { favorites, addFavorite, removeFavorite } = useContext(FavoritesContext);
 
   const isFavorite = favorites.some((favorite) => favorite.id === id);
+  const thumbnailPath = thumbnail.path;
+
+  const imgSrc = thumbnailPath.includes('image_not_available')
+    ? `${marvelCharacters}`
+    : `${thumbnailPath}.${thumbnail.extension}`;
 
   const handleOpen = () => {
     setLoading(true);
@@ -33,7 +39,7 @@ const Card = ({ character }) => {
   return (
     <>
       <CardWrapper onClick={handleOpen}>
-        <CardImageWrapper src={`${thumbnail.path}.${thumbnail.extension}`} alt={name} />
+        <CardImageWrapper src={imgSrc} alt={name} />
         <CardTitleWrapper>{name}</CardTitleWrapper>
         <CardStarWrapper onClick={handleStarClick}>
           <Star fill={isFavorite} />
